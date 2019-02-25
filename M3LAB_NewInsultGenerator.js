ecf1468dee9085420fca565fcce43de339e1fc3d
KeymoniSS <43156164+KeymoniSS@users.noreply.github.com>
2019-02-25 14:23:12 -0500

    Add files via upload

---
+++ M3LAB_NewInsultGenerator.js
@@ -0,0 +1,53 @@
+<!--
+WEB 151
+M3LAB_NewInsultGenerator
+sakilslack
+2/25/19
+-->
+
+<html>
+	<head>
+	<style>
+  div {
+  border: 1px solid gray;
+  padding: 8px;
+  background-color: #2D292E;
+  color: #FFFFFF;
+  font-family: "Comic Sans MS", cursive, sans-serif;
+  font-size: 2.5em;
+}
+
+h1 {
+  text-align: center;
+  text-transform: uppercase;
+  color: #A9F3FF;
+  text-shadow: 3px 2px #FFC3A9;
+  font-family: "Courier New", Courier, monospace;
+  font-size: 3.5em;
+}
+
+p {
+  text-indent: 50px;
+  text-align: justify;
+  letter-spacing: 3px;
+  color: #FFFFFF;
+  
+}
+a {
+  text-decoration: none;
+  color: #008CBA;
+  
+}
+	</style>
+	</head>
+	
+	<body>
+	<div>
+	<h1> Insult Generator </h1>
+	<center>
+	<script src = "scripts/script.js">
+	</script>
+	</center>
+	</div>
+	</body>
+</html>
